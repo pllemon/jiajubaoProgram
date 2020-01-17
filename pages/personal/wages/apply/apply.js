ac9af8cf1d017852ad5f3a2469f292a65bafc5a1
pllemon <37794820+pllemon@users.noreply.github.com>
2020-01-17 15:04:06 +0800

    提交

--- pages/personal/wages/apply/apply.js
+++ pages/personal/wages/apply/apply.js
@@ -1,30 +1,55 @@
 const app = getApp()
+let num = 0
 
 Page({
   data: {
-    type: '',
-    query1: {
-      pay_status: 0,
+    query: {
+      pay_status: 1,
       pay_status_code: 'GETONE',
       cmorderstatus: 4
     },
-    query2: {
-      pay_status: 1,
-      pay_status_code: 'GETTOW',
-      cmorderstatus: 4
+    list: [],
+
+    form: {
+      order_id: '',
+      pay_status_code: '',
+      pay_name: '',
+      pay_type: ''
     },
-    list: []
+
+    isPopup: false,
+    radio: '1'
   },
   onLoad(params) {
-    this.setData({
-      type: params.type
-    })
-    if (this.data.type == 1) {
-      this.getOrderList(this.data.query1)
-    } else {
-      this.getOrderList(this.data.query2)
+    let query = this.data.query
+    if (params.type == 2) {
+      query.pay_status_code = 'GETTOW'
     }
+    this.getOrderList(this.data.query)
   },
+  
+  openPopup() {
+    this.setData({ 
+      isPopup: true 
+    })
+  },
+  
+  closePopup() {
+    this.setData({ 
+      isPopup: false 
+    })
+  },
+
+  onChange(e) {
+    common.changeInput(this, e)
+  },
+
+  changeType(e) {
+    this.setData({
+      radio: e.detail
+    })
+  },
+
 
   getOrderList(data = {}) {
     let that = this;
@@ -38,4 +63,44 @@ Page({
       }
     })
   },
+
+  applySingle(order_id) {
+    let that = this;
+    let data = that.data.form;
+    data.order_id = order_id;
+    console.log(this.data.form);
+    return false;
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
+    app.request({
+      url: '/craftsmancashout',
+      data,
+      hideLoading: true,
+      complete: function() {
+        num = num + 1;
+        if (num == that.data.list.length) {
+          wx.hideLoading()
+          wx.showToast({
+            title: '提交成功',
+            icon: 'success',
+            duration: 1000
+          })
+          setTimeout(() => {
+            wx.redirectTo({
+              url: '/pages/personal/wages/applyRecord/applyRecord'
+            })
+          }, 1000)
+        }
+      }
+    })
+  },
+
+  formSubmit() {
+    num = 0;
+    this.data.list.forEach(item => {
+      this.applySingle(item.order_id) 
+    })
+  }
 })
