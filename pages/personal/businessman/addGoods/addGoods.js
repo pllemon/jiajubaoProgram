dcece2a46e94d654314dc96e37dfa6ddaecc90d0
pllemon <nageshui071011>
2020-06-10 01:17:23 +0800

    提交

--- pages/personal/businessman/addGoods/addGoods.js
+++ pages/personal/businessman/addGoods/addGoods.js
@@ -3,24 +3,10 @@ const common = require('../../../../utils/common.js');
 
 Page({
   data: {
-    statusArr: [
-      {
-        id: 'TG',
-        name: '成功'
-      },
-      {
-        id: 'BH',
-        name: '失败'
-      }
-    ],
-    statusIdx: 0,
-
     form: {
-      status: '',
-      appo_time: '',
-      htremark: '',
-      total_price: '',
-      bjimg: ''
+      goods_name: '',
+      price: '',
+      goods_image: ''
     },
     imgArr: []
   },
@@ -29,69 +15,49 @@ Page({
     let that = this;
   },
 
-  // 更改值
-  bindChange (e) {
-    this.setData({
-      [e.target.dataset.label]: e.detail.value
-    })
-  },
-
   formSubmit(e) {
     let that = this;
     let form = this.data.form;
-    let pass = true;
-    form.status = this.data.statusArr[this.data.statusIdx].id;
     
-    if (this.data.statusIdx == 0) { // 通过
-      if (!form.appo_time) {
-        app.showModal('请选择开工时间');
-        pass = false;
-        return false;
-      }
-      if (!form.total_price) {
-        app.showModal('请输入订单总价');
-        pass = false;
-        return false;
-      }
-      if (!this.data.imgArr.length) {
-        app.showModal('请上传报价单');
-        pass = false;
-        return false;
-      }
-    } else {
-      form.appo_time = ''
-      form.total_price = ''
+    if (!form.goods_name) {
+      app.showModal('请输入商品名称');
+      return false;
     }
-
-    if (!pass) {
+    if (!form.price) {
+      app.showModal('请输入商品价格');
       return false;
     }
 
     wx.showLoading({
       title: '上传中',
     })
-    common.uploadImgs('uploadordersimg', this.data.imgArr, function (res) {
-      wx.hideLoading();
-      form.bjimg = res[0].url;
-      that.setData({
-        form
-      })
-      that.submitFn();
-    })
-  },
-
-  submitFn() {  
-    console.log(this.data.form)
-    return false;
-    app.request({
-      url: '/markorder',
-      data: this.data.form,
+    wx.uploadFile({
+      url: 'http://47.106.100.144/businessuploadgoods',
+      filePath: this.data.imgArr[0].url,
+      name: 'goods_image',
+      formData: form,
+      header: {
+        'content-type': 'multipart/form-data',
+        'cookie': app.globalData.session
+      },
       success: function(data) {
-        app.successToast('创建订单成功', function(){
-          wx.reLaunch({
-            url: '/pages/order/list/list?personType=0&showNav=1'
+        let odata = JSON.parse(data.data)
+        if (odata.success) {
+          app.successToast('提交成功', function(){
+            let pages = getCurrentPages();
+            let beforePage = pages[pages.length - 2];
+            beforePage.selectComponent("#list").getData(1);
+            wx.navigateBack();        
           })
-        })
+        } else {
+          app.showModal(odata.message);
+        }
+      },
+      fail: function(err) {
+        console.log(err)
+      },
+      complete: function() {
+        wx.hideLoading();
       }
     })
   },
@@ -104,10 +70,5 @@ Page({
   },
   onChange(e) {
     common.changeInput(this, e)
-  },
-  changeSwitch(event) {
-    this.setData({
-      checked: event.detail
-    })
-  },
+  }
 })
--- pages/personal/businessman/goods/goods.js
+++ pages/personal/businessman/goods/goods.js
@@ -3,21 +3,15 @@ const common = require('../../../../utils/common.js');
 
 Page({
   data: {
-    list: [
-      {
-        id: '1111',
-        name: 'xxxxxxxxx',
-        price: 2.5,
-        number: 0
-      },
-      {
-        id: '2222',
-        name: 'yyyyyyyy',
-        price: 5.5,
-        number: 0
-      }
-    ],
-    sum: 0
+    list: [],
+    requestUrl: '/businessgoodslist',
+    query: {},
+  },
+
+  changeList(e) {
+    this.setData({
+      list: e.detail
+    })
   },
 
   addGoods() {
