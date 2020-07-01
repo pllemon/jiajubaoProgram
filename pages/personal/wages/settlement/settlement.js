68b693842c09d36d957ea29360621511f68123c3
pllemon <nageshui071011>
2020-07-02 02:08:31 +0800

    更新

--- pages/personal/wages/settlement/settlement.js
+++ pages/personal/wages/settlement/settlement.js
@@ -8,12 +8,69 @@ Page({
     list: [],
     sumprofit: 0,
     query: {},
+
+    isPopup: false,
+    radio: '2',
+    form: {
+      money: '',
+      pay_name: '',
+      pay_type: '',
+      pay_username: ''
+    },
   },
 
   changeList(e) {
     this.setData({
       list: e.detail.list,
       sumprofit: e.detail.data ? e.detail.data.sumprofit : 0
+    })
+  },
+
+  openPopup() {
+    this.setData({ 
+      isPopup: true 
+    })
+  },
+  
+  closePopup() {
+    this.setData({ 
+      isPopup: false 
+    })
+  },
+
+  onChange(e) {
+    common.changeInput(this, e)
+  },
+
+  changeType(e) {
+    this.setData({
+      radio: e.detail
+    })
+  },
+
+  apply() {
+    let that = this;
+    let data = that.data.form;
+    data.pay_type = this.data.radio;
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
+    app.request({
+      url: '/userwithdrawal',
+      data,
+      hideLoading: true,
+      complete: function() {
+        num = num + 1;
+        if (num == that.data.list.length) {
+          wx.hideLoading()
+          app.successToast('提交成功', function(){
+            wx.redirectTo({
+              url: '/pages/personal/wages/settlementRecord/settlementRecord'
+            })
+          })
+        }
+      }
     })
   }
 })
