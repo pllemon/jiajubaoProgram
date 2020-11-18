b62d0cfeddd77a275144a56653d18b2f9e095f6f
pllemon <37794820+pllemon@users.noreply.github.com>
2020-11-18 18:28:24 +0800

    111

--- pages/personal/feedback/feedback.js
+++ pages/personal/feedback/feedback.js
@@ -2,18 +2,21 @@ const app = getApp()
 
 Page({
   data: {
-    order_sn: ''
+    order_sn: '',
+    order_id: ''
   },
 
   onLoad(params) {
     this.setData({
-      order_sn: params.order_sn
+      order_sn: params.order_sn,
+      order_id: params.order_id
     })
   },
 
   formSubmit(e) {  
     let formData = e.detail.value;
     formData.order_sn = this.data.order_sn;
+    formData.order_id = this.data.order_id;
     app.request({
       url: '/feedback',
       data: formData,
